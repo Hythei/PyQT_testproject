c78625b651df1cd4918537407807fba86f6cda71
Hythei <heikki.hyttinen16@gmail.com>
2026-04-22 22:26:28 +0300

    Progress

--- app.py
+++ app.py
@@ -8,24 +8,29 @@ class MainWindow(QMainWindow):
         self.button_is_checked = True
         self.setWindowTitle("My App")
 
-        button = QPushButton("Click Me")
-        button.setCheckable(True)
+        self.button = QPushButton("Click Me")
+        self.button.setCheckable(True)
 #        button.clicked.connect(self.the_button_was_clicked)
-        button.clicked.connect(self.the_button_was_toggled)
-        button.setChecked(self.button_is_checked)
+#        button.clicked.connect(self.the_button_was_toggled)
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
 
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
 
 #    def the_button_was_clicked(self):
 #        print("the button was clicked")
 
-    def the_button_was_toggled(self, checked):
+#    def the_button_was_toggled(self, checked):
 #        print(f"Checked: {checked}")
-        self.button_is_checked = checked
+#        self.button_is_checked = checked
+#        print(self.button_is_checked)
+
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
         print(self.button_is_checked)
 
-app = QApplication(sys.argv)
 
+app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 

50720ef47f30212415d7fdab8ff45c5c2bfdda37
Hythei <heikki.hyttinen16@gmail.com>
2026-04-23 23:22:42 +0300

    moving onto widgets

--- app.py
+++ app.py
@@ -9,14 +9,27 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.show()
+
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.on_context_menu)
 
 
-    def contextMenuEvent(self, e):
+    def on_context_menu(self, pos):
         context = QMenu(self)
-        context.addAction(QAction("test 1", self))
-        context.addAction(QAction("test 2", self))
-        context.addAction(QAction("test 3", self))
-        context.exec(e.globalPos())
+        action1 = QAction("test 1", self)
+        action2 = QAction("test 2", self)
+        action3 = QAction("test 3", self)
+
+        action1.triggered.connect(lambda: print("test 1 chosen"))
+        action2.triggered.connect(lambda: print("test 2 chosen"))
+        action3.triggered.connect(lambda: print("test 3 chosen"))
+
+        context.addAction(action1)
+        context.addAction(action2)
+        context.addAction(action3)
+
+        context.exec(self.mapToGlobal(pos))
 
 app = QApplication(sys.argv)
 window = MainWindow()

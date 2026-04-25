a59ecb731709ad443f124a003e4e1b250679ac00
Hythei <heikki.hyttinen16@gmail.com>
2026-04-25 23:51:46 +0300

    moving onto Widgets

--- app.py
+++ app.py
@@ -1,38 +1,67 @@
 import sys
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QTextEdit, QMenu
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
-
+# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
-        self.show()
 
-        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.on_context_menu)
+        self.setWindowTitle("Widgets App")
 
+        layout = QVBoxLayout()
+        widgets = [
+            QCheckBox,
+            QComboBox,
+            QDateEdit,
+            QDateTimeEdit,
+            QDial,
+            QDoubleSpinBox,
+            QFontComboBox,
+            QLCDNumber,
+            QLabel,
+            QLineEdit,
+            QProgressBar,
+            QPushButton,
+            QRadioButton,
+            QSlider,
+            QSpinBox,
+            QTimeEdit,
+        ]
 
-    def on_context_menu(self, pos):
-        context = QMenu(self)
-        action1 = QAction("test 1", self)
-        action2 = QAction("test 2", self)
-        action3 = QAction("test 3", self)
+        for w in widgets:
+            layout.addWidget(w())
 
-        action1.triggered.connect(lambda: print("test 1 chosen"))
-        action2.triggered.connect(lambda: print("test 2 chosen"))
-        action3.triggered.connect(lambda: print("test 3 chosen"))
+        widget = QWidget()
+        widget.setLayout(layout)
 
-        context.addAction(action1)
-        context.addAction(action2)
-        context.addAction(action3)
-
-        context.exec(self.mapToGlobal(pos))
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
 
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
-
 app.exec()

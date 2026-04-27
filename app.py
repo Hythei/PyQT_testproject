954e4ae33a286e6d04db033a3f08e14d614f1614
Hythei <heikki.hyttinen16@gmail.com>
2026-04-27 23:48:54 +0300

    checkbox

--- app.py
+++ app.py
@@ -22,13 +22,15 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My App")
 
-        label = QLabel(self)
-        pixmap = QPixmap("pictures/strikekot.jpg")
-        label.setPixmap(pixmap)
-        label.setAlignment(
-            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter
-        )
-        self.setCentralWidget(label)
+        widget = QCheckBox("This is a checkbox")
+        widget.setCheckState(Qt.CheckState.Checked)
+
+        widget.stateChanged.connect(self.show_state)
+        self.setCentralWidget(widget)
+
+    def show_state(self, s):
+        print(s == Qt.CheckState.Checked.value)
+        print(s)
 
 app = QApplication(sys.argv)
 w = MainWindow()
